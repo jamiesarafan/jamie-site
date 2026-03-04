34961a039b82c4b69145015b82d6d440edf1d430
jamiesarafan <jamie.sarafan.mt@gmail.com>
2026-03-03 18:22:03 -0800

    Update site.ts

--- src/content/site.ts
+++ src/content/site.ts
@@ -29,7 +29,8 @@ export const site = {
       {
         title:
           "The Women and the Word: Serpent Handling, Devotional Writing, and the Women in the Church of God, 1914–1936",
-        venue: "Journal of Southern Religion, 2025",
+        venue: "Journal of Southern Religion",
+        year: "2025",
         link: "https://jsreligion.org/vol24/sarafan/",
       },
     ],
